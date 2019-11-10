c126764e258beb650da16dff0edaea05fa230803
Krzysztof Wesolowski <krzysztof.wesolowski@rainlabs.pl>
2019-11-10 16:31:30 +0100

    Move handling setters to common our

--- nodes/miio-purifier-out.js
+++ nodes/miio-purifier-out.js
@@ -12,41 +12,7 @@ module.exports = function (RED) {
         }
         async SetUp() {
             await this.connect();
-            await this.outputSetup(this.handlePayload, this.handlePayloadRaw)
-        }
-
-        async handlePayloadRaw(payload_raw) {
-            const known_properties_setters = {
-                "power": "set_power",
-                "child_lock": "set_child_lock",
-                "mode": "set_mode",
-                "favorite_level": "set_level_favorite",
-            };
-            const known_properties = ["power", "child_lock", "mode", "favorite_level"];
-            const change_properties = Object.keys(payload_raw).filter(k => known_properties.includes(k));
-
-            const current_values = await this.device.loadProperties(change_properties);
-            for (let i = 0; i < change_properties.length; i++) {
-                const prop = change_properties[i];
-                const current_value = current_values[prop];
-                const target_value = payload_raw[prop];
-                const set_cmd = known_properties_setters[prop];
-
-                if (current_value !== target_value) {
-                    try {
-                        const result = await this.device.call(set_cmd, [target_value]);
-                        if (result[0] === "ok") {
-                            this.debug(`Changing "${prop}" from ${current_value} to "${target_value}" succeeded`);
-                        } else {
-                            this.debug(new Error(result[0]));
-                        }
-                    } catch (err) {
-                        this.error(`Failed to ${set_cmd} to ${target_value} due to `+ err);
-                    }
-                } else {
-                    this.debug(`Skip calling ${set_cmd} as ${prop} is already ${current_value}`)
-                }
-            }
+            await this.outputSetup(this.handlePayload)
         }
 
         async handlePayload(payload) {
